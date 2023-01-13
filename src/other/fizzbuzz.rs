6f4aedbf461318f9c569719f3c262d9e497096e3
James <11761543+jagreenwood1@users.noreply.github.com>
2023-01-13 20:09:38 +0000

    fix fizzbuzz

--- src/other/fizzbuzz.rs
+++ src/other/fizzbuzz.rs
@@ -9,11 +9,12 @@ fn fizzbuzz_to(n: u32) -> Vec<String> {
 }
 
 fn fizzbuzz(n: u32) -> String {
+    let binding = n.to_string();
     let result_str = match (is_divisible_by(n, 3), is_divisible_by(n, 5)) {
         (true, true) => "fizzbuzz",
         (true, false) => "fizz",
         (false, true) => "buzz",
-        (false, false) => n.to_string().as_str(),
+        (false, false) => &binding.as_str(),
     };
 
     return result_str.to_string();
@@ -33,6 +34,12 @@ mod tests {
     use crate::other::fizzbuzz::fizzbuzz_to;
 
     #[test_case(1, &["1".to_string()])]
+    #[test_case(5, &["1".to_string(),
+                    "2".to_string(),
+                    "fizz".to_string(),
+                    "4".to_string(),
+                    "buzz".to_string()
+    ])]
     fn it_works(number: u32, expected: &[String]) {
         let result = fizzbuzz_to(number);
         assert_eq!(result, expected);
