ee5ad9338c764ac5fce3ab5d0050c8f2bdb8fb6e
James <11761543+jagreenwood1@users.noreply.github.com>
2023-01-13 10:21:38 +0000

    add fizzbuzzz

---
+++ src/other/fizzbuzz.rs
@@ -0,0 +1,36 @@
+fn fizzbuzz_to(n: u32) {
+    // `-> ()` is normally omitted
+    for n in 1..=n {
+        fizzbuzz(n);
+    }
+}
+
+fn fizzbuzz(n: u32) -> () {
+    // No return value means returning the unit type `()`
+    match (is_divisible_by(n, 3), is_divisible_by(n, 5)) {
+        (true, true) => println!("fizzbuzz"),
+        (true, false) => println!("fizz"),
+        (false, true) => println!("buzz"),
+        (false, false) => println!("{n}"),
+    }
+}
+
+fn is_divisible_by(lhs: u32, rhs: u32) -> bool {
+    if rhs == 0 {
+        return false; // Corner case, early return
+    }
+    lhs % rhs == 0 // The last expression is the return value
+}
+
+#[cfg(test)]
+mod tests {
+    use test_case::test_case;
+
+    use crate::other::fizzbuzz::fizzbuzz_to;
+
+    #[test_case(1, &[1])]
+    fn it_works(number: u64, expected: &[usize]) {
+        let result = fizzbuzz_to(number);
+        assert_eq!(result, expected);
+    }
+}
