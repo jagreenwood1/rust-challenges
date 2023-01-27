759a4a367fc73d38f6e1f8b9a9759d09d0f0d525
James <11761543+james-greenwood@users.noreply.github.com>
2023-01-27 10:57:26 +0000

    DNA

--- src/other/sandbox.rs
+++ src/other/sandbox.rs
@@ -1,39 +1,31 @@
-fn high_and_low_a(numbers: &str) -> String {
-    let mut high = -f64::INFINITY;
-    let mut low = f64::INFINITY;
+// "A" and "T" are complements of each other, as "C" and "G".
 
-    numbers.split(' ').for_each(|num_str| {
-        let num_int = num_str.parse::<f64>().unwrap();
-        if num_int > high {
-            high = num_int
-        }
-
-        if num_int < low {
-            low = num_int
-        }
-    });
-
-    format!("{high} {low}")
+fn dna_strand(dna: &str) -> String {
+    dna.chars()
+        .map(|c| match c {
+            'A' => 'T',
+            'T' => 'A',
+            'C' => 'G',
+            'G' => 'C',
+            _ => '_',
+        })
+        .collect()
 }
 
-fn high_and_low(numbers: &str) -> String {
-    use std::cmp;
+#[cfg(test)]
+mod tests {
+    use super::dna_strand;
 
-    let (high, low) = numbers
-        .split_whitespace()
-        .map(|x| x.parse::<i32>().unwrap())
-        .fold((i32::min_value(), i32::max_value()), |(max, min), x| {
-            (cmp::max(max, x), cmp::min(min, x))
-        });
-    format!("{high} {low}")
-}
+    fn dotest(s: &str, expected: &str) {
+        let actual = dna_strand(s);
+        eprintln!("With dna = \"{s}\"\nExpected \"{expected}\" but got \"{actual}\"");
+        assert!(actual == expected);
+    }
 
-#[test]
-fn example_test_1() {
-    assert_eq!("42 -9", high_and_low("8 3 -5 42 -1 0 0 -9 4 7 4 -4"));
-}
-
-#[test]
-fn example_test_2() {
-    assert_eq!("3 1", high_and_low("1 2 3"));
+    #[test]
+    fn fixed_tests() {
+        dotest("AAAA", "TTTT");
+        dotest("ATTGC", "TAACG");
+        dotest("GTAT", "CATA");
+    }
 }
