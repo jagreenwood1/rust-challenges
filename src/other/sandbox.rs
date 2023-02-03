d5db02ab22689e83e77326456e8b6bffec51585b
James <11761543+james-greenwood@users.noreply.github.com>
2023-02-03 10:32:16 +0000

    quicksort tweak

--- src/other/sandbox.rs
+++ src/other/sandbox.rs
@@ -4,20 +4,6 @@ fn return_sorted_list<T: Ord>(input: &mut [T]) -> &mut [T] {
     input
 }
 
-fn run_quick_sort() {
-    println!("Sort numbers ascending");
-    let mut numbers = [4, 65, 2, -31, 0, 99, 2, 83, 782, 1];
-    println!("Before: {:?}", numbers);
-    quick_sort(&mut numbers);
-    println!("After:  {:?}\n", numbers);
-
-    println!("Sort strings alphabetically");
-    let mut strings = ["beach", "hotel", "airplane", "car", "house", "art"];
-    println!("Before: {:?}", strings);
-    quick_sort(&mut strings);
-    println!("After:  {:?}\n", strings);
-}
-
 pub fn quick_sort<T: Ord>(arr: &mut [T]) {
     let len = arr.len();
     _quick_sort(arr, 0, (len - 1) as isize);
